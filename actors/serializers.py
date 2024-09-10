a0c686ae0a1336a4fa467d962c5566ad83605ec3
Evandro Neto <evandrorpneto@gmail.com>
2024-09-09 22:29:38 -0300

    Create crud views from actors, serializer and urls

---
+++ actors/serializers.py
@@ -0,0 +1,10 @@
+from dataclasses import fields
+from rest_framework import serializers
+from actors.models import Actor
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Actor
+        fields = '__all__'
--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
-
+from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('genres/', GenreCreateListView.as_view(), name='genre-list'),
-    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name="genre-detail-view")
+    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(),
+         name="genre-detail-view"),
+    
+    path('actors/', ActorCreateListView.as_view(), name='actor-create-list'),
+    path('actors/<int:pk>/', ActorRetrieveUpdateDestroyAPIView.as_view(),
+         name='actor-create-list'),
 ]
