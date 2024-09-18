2a9be1a097333ad7c51f0ee4753a151de6bdb174
Evandro Neto <evandrorpneto@gmail.com>
2024-09-17 22:04:21 -0300

    implements validations on Movie serializer

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
 from movies.models import Movie
 
-class MovieSerializer(serializers.ModelSerializer):
+class MovieModelSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Movie
         fields = '__all__'
+        
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1990.')
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError('Resumo não deve ser maior que 200 caracteres.')
+        return value
+    
+    def validate_title(self, value):
+        value = value.upper()
+        return value
