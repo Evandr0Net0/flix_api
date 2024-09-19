0ff525fe906f15f9de5d22da48946cc967944bad
Evandro Neto <evandrorpneto@gmail.com>
2024-09-18 23:57:39 -0300

    create serializer methods calculate

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,11 +1,24 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movie
 
 class MovieModelSerializer(serializers.ModelSerializer):
+    # Campo além dos que já existem no model
+    rate = serializers.SerializerMethodField(read_only=True)
     
     class Meta:
         model = Movie
         fields = '__all__'
+    
+    # Método para calculo do rate    
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+        
+        if rate:
+            return round(rate, 1)
+
+        return None
+        
         
     def validate_release_date(self, value):
         if value.year < 1990:
