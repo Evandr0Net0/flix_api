afd00de6db530e2171cafb2984c214e2ab23b747
Evandro Neto <109848290+Evandr0Net0@users.noreply.github.com>
2024-11-06 22:22:20 -0300

    aa

--- genres/permissions.py
+++ genres/permissions.py
@@ -1,6 +1,13 @@
+import re
 from rest_framework import permissions
 
 class GenrePermissionsClass(permissions.BasePermission):
     
     def has_permission(self, request, view):
-        return True
+        if request.method == 'GET':
+            
+            # padrão: [nome da app].view_[nome do model]
+            return request.user.has_permission('genres.view_genre')
+        
+        return False
+    
