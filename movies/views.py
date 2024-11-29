523a424e31440b73a3eeac13cfea2bed8ee7242f
Evandro Neto <evandrorpneto@gmail.com>
2024-11-28 23:14:04 -0300

    Update views.py

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,5 @@
 from django.db.models import Avg
-from urllib import response
-from rest_framework import generics, views, status
+from rest_framework import generics, views, status, response
 from rest_framework.permissions import IsAuthenticated
 from app.permissions import GlobalDefaultPermissions
 from movies.models import Movie
@@ -38,23 +37,7 @@ class MovieStatsView(views.APIView):
 
     def get(self, request):
 
-        total_movies = self.queryset.count()
-        # dunder para acessar o nome da coluna do model genre
-        movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))
-        total_reviews = Review.objects.count()
-        average_stars = Review.objects.aggregate(avg_stars=Avg('stars'))['avg_stars']
-
-        movies_stats = {
-            'total_movies': total_movies,
-            'movies_by_genre': movies_by_genre,
-            'total_reviews': total_reviews,
-            'average_stars': round(average_stars, 1) if average_stars else 0,
-        }
-
-        serializer = MovieStatsSerializer(data=movies_stats)
-        serializer.is_valid(raise_exception=True)
-
         return response.Response(
-            data=serializer.validated_data,
+            data={'message': 'Funcionou'},
             status=status.HTTP_200_OK,
         )
