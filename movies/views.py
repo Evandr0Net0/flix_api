40e0129ec0b56a44dc03dddc4306d21d04d93913
Evandro Neto <evandrorpneto@gmail.com>
2024-11-28 23:02:50 -0300

    Update views.py

--- movies/views.py
+++ movies/views.py
@@ -6,6 +6,7 @@ from app.permissions import GlobalDefaultPermissions
 from movies.models import Movie
 from movies.serializers import MovieModelSerializer, MovieStatsSerializer, MovieListDetailSerializer
 from reviews.models import Review
+from django.db.models import Count
 
 
 class MovieCreateListView(generics.ListCreateAPIView):
@@ -36,23 +37,25 @@ class MovieStatsView(views.APIView):
     queryset = Movie.objects.all()
 
     def get(self, request):
+
         total_movies = self.queryset.count()
         # dunder para acessar o nome da coluna do model genre
-        # movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))
+        movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))
         total_reviews = Review.objects.count()
         average_stars = Review.objects.aggregate(avg_stars=Avg('stars'))['avg_stars']
 
         movies_stats = {
             'total_movies': total_movies,
-            # 'movies_by_genre':  movies_by_genre,
+            'movies_by_genre': movies_by_genre,
             'total_reviews': total_reviews,
             'average_stars': round(average_stars, 1) if average_stars else 0,
         }
 
         serializer = MovieStatsSerializer(data=movies_stats)
-        serializer.is_valid(raise_exception=True)
+        # serializer.is_valid(raise_exception=True)
 
         return response.Response(
             data=serializer.validated_data,
             status=status.HTTP_200_OK,
         )
+
